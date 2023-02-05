08174a8b150dc5bdbd96f25a7c2584d8ff4fcc2b
KeremTancan <tancankerem149@gmail.com>
2023-02-05 14:11:04 +0300

    enemy and game name added

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -10,7 +10,7 @@ public class EnemyPatrol : MonoBehaviour {
         if(patrolDestination == 0){
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[0].position, moveSpeed*Time.deltaTime);
             if(Vector2.Distance(transform.position, patrolPoints[0].position) < .2f){
-                transform.localScale = new Vector3(1,1,1);
+                transform.localScale = new Vector3(6,6,1);
                 patrolDestination = 1;
 
             }
@@ -19,7 +19,7 @@ public class EnemyPatrol : MonoBehaviour {
          if(patrolDestination == 1){
             transform.position = Vector2.MoveTowards(transform.position, patrolPoints[1].position, moveSpeed*Time.deltaTime);
             if(Vector2.Distance(transform.position, patrolPoints[1].position) < .2f){
-                transform.localScale = new Vector3(-1,-1,-1);
+                transform.localScale = new Vector3(-6,6,-1);
                 patrolDestination = 0;
                 
             }
