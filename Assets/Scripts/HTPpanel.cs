5fed3d7e30c39c65597da606240d81bc685073b4
KeremTancan <tancankerem149@gmail.com>
2023-02-20 15:09:12 +0300

    tutorial level update

---
+++ Assets/Scripts/HTPpanel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HTPpanel : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+
+            Panel.SetActive(!isActive);
+        }
+
+    }
+
+
+
+}
